9210d681221153ed2360c41ee1965dc9826079fd
princeedwardraj-cpu <princeedwardraj@gmail.com>
2026-07-26 20:29:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const PASSWORD = "EVENT2026";
+
+const DRIVE_LINK = "https://drive.google.com/drive/folders/1tg2SYVi5RMYL3jy-xgWjGJsy7BrGjSpy?usp=sharing";
+
+function checkPassword(){
+
+const pass=document.getElementById("password").value;
+
+if(pass===PASSWORD){
+
+window.location.href=DRIVE_LINK;
+
+}else{
+
+document.getElementById("error").innerHTML="❌ Wrong Password";
+
+}
+
+}
